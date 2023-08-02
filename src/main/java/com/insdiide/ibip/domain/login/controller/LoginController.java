7bd2683af1a45b76ab883d6d6c7583b6406b0702
ldh <ldh.insideinfo@gmail.com>
2023-08-02 11:15:25 +0900

    CustomMessage 테스트

--- src/main/java/com/insdiide/ibip/domain/login/controller/LoginController.java
+++ src/main/java/com/insdiide/ibip/domain/login/controller/LoginController.java
@@ -4,16 +4,21 @@ import com.insdiide.ibip.domain.login.vo.LoginVO;
 import com.insdiide.ibip.domain.login.vo.ResponseVO;
 import com.microstrategy.web.objects.WebIServerSession;
 import com.microstrategy.web.objects.WebObjectsFactory;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+@Log4j2
 @Controller
 public class LoginController {
 
@@ -24,21 +29,24 @@ public class LoginController {
          * TODO: Session값을 통해 이미 로그인을 한 사용자는 메인페이지로 redirect 시킬 것
          *
          * **/
+        log.info("응?");
         return "/login/login";
 
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody @Valid LoginVO loginVO, BindingResult bindingResult, HttpServletRequest request){
-        System.out.println("로그인 ID : " + loginVO.getId());
-        System.out.println("로그인 PWD : " + loginVO.getPwd());
+    @ResponseBody
+    public ResponseEntity<?> login(@RequestBody @Valid LoginVO loginVO, BindingResult bindingResult, HttpServletRequest request){
+        log.info("로그인 ID : " + loginVO.getId());
+        log.info("로그인 PWD : " + loginVO.getPwd());
 
         if(bindingResult.hasErrors()){
-            System.out.println(bindingResult.getFieldErrors());
+            log.info(bindingResult.getFieldErrors().get(0).getDefaultMessage());
+            log.info(bindingResult.getFieldErrors().get(0).getCode());
         }
 
         //응답 VO 생성
-        ResponseVO responseVO = new ResponseVO();
+        ResponseVO responseVO = ResponseVO.builder().code("S00").msg("굳").build();
 
         WebObjectsFactory factory = WebObjectsFactory.getInstance();
         WebIServerSession serverSession = factory.getIServerSession();
@@ -51,7 +59,7 @@ public class LoginController {
         serverSession.setAuthMode(1);
         serverSession.setLogin("administrator"); // User ID
         serverSession.setPassword(""); // Password
-        return "/aa";
+        return new ResponseEntity<>(responseVO, HttpStatus.NOT_FOUND);
     }
 
 
--- src/main/java/com/insdiide/ibip/domain/login/vo/ResponseVO.java
+++ src/main/java/com/insdiide/ibip/domain/login/vo/ResponseVO.java
@@ -1,5 +1,10 @@
 package com.insdiide.ibip.domain.login.vo;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ResponseVO {
 
     private String code;
