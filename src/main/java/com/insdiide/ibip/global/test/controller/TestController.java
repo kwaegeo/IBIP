087c580769be642458003a6e0b44950ed91269a7
ldh <ldh.insideinfo@gmail.com>
2023-09-26 17:53:22 +0900

    임시 iframe 오픈

---
+++ src/main/java/com/insdiide/ibip/global/test/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.insdiide.ibip.global.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+
+    @GetMapping("/test2")
+    private String test2(){
+
+        return "/test2";
+    }
+
+    @GetMapping("/test3")
+    private String test3(){
+
+        return "/test3";
+    }
+}
