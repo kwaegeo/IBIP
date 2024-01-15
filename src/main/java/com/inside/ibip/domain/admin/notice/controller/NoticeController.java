f71fee5b9f453d200737e7fc59329de5c935feb0
kwaegeo <66067521+kwaegeo@users.noreply.github.com>
2024-01-16 05:39:42 +0900

    공지사항 주석 및 코드정리

--- src/main/java/com/inside/ibip/domain/admin/notice/controller/NoticeController.java
+++ src/main/java/com/inside/ibip/domain/admin/notice/controller/NoticeController.java
@@ -6,18 +6,32 @@ import com.inside.ibip.global.exception.CustomException;
 import com.inside.ibip.global.utils.ComUtils;
 import com.inside.ibip.global.vo.ResVO;
 import com.microstrategy.web.objects.WebObjectsException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @FileName     : NoticeController.java
+ * @Date         : 2023.12.01
+ * @Author       : 이도현
+ * @Description  : 공지사항 Controller, 공지사항 리스트 조회, 정보 조회 등
+ * @History
+ * =======================================================
+ *   DATE			AUTHOR			NOTE
+ * =======================================================
+ *   2023.12.01     이도현         최초작성
+ *
+ */
+@Log4j2
 @Controller
+@RequestMapping("/notice")
 public class NoticeController {
 
     @Autowired
@@ -26,9 +40,21 @@ public class NoticeController {
     @Autowired
     private ComUtils comUtils;
 
-    @PostMapping("/getNoticeList")
+    /**
+     * 공지사항 리스트 조회
+     * @Method Name   : getNoticeList
+     * @Date / Author : 2023.12.01  이도현
+     * @param request request 객체
+     * @param response response 객체
+     * @return 그룹 리스트 객체
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
+    @PostMapping("/get/list")
     @ResponseBody
-    public List<NoticeVO> getNoticeList(HttpServletRequest request, HttpServletResponse response) throws WebObjectsException {
+    public List<NoticeVO> getNoticeList(HttpServletRequest request, HttpServletResponse response){
 
         //1. 사용자 세션 (MSTR) 유효성 검사
 
@@ -36,90 +62,148 @@ public class NoticeController {
         String mstrSessionId = (String) httpSession.getAttribute("mstrSessionId");
 
         //1. 세션 체크
-        try{
-            comUtils.sessionCheck(mstrSessionId, request, response);
-        }catch(CustomException ex){
-            throw ex;
-        }
+        comUtils.sessionCheck(mstrSessionId, request, response);
 
         List<NoticeVO> noticeList = noticeService.getNoticeList();
 
         return noticeList;
     }
 
-    @GetMapping("/noticeAdd")
-    public String noticeAdd(HttpServletRequest request, HttpServletResponse response, Model model){
+    /**
+     * 공지사항 생성 페이지 조회
+     * @Method Name   : noticeAdd
+     * @Date / Author : 2023.12.01  이도현
+     * @param request request 객체
+     * @param response response 객체
+     * @return 공지사항 생성 페이지
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
+    @GetMapping("/add")
+    public String noticeAdd(HttpServletRequest request, HttpServletResponse response){
+
+        HttpSession httpSession = request.getSession(true);
+        String mstrSessionId = (String) httpSession.getAttribute("mstrSessionId");
+
+        //1. 세션 체크
+        comUtils.sessionCheck(mstrSessionId, request, response);
 
         return "/admin/notice/add";
     }
 
-    @GetMapping("/noticeInfo")
-    public String noticeInfo(@RequestParam String noticeId, HttpServletRequest request, HttpServletResponse response, Model model) throws WebObjectsException {
-        System.out.println(noticeId);
-        //1. 사용자 세션 (MSTR) 유효성 검사
+    /**
+     * 공지사항 정보 조회
+     * @Method Name   : getNoticeInfo
+     * @Date / Author : 2023.12.01  이도현
+     * @param noticeId 조회할 공지사항의 Id
+     * @param request request 객체
+     * @param response response 객체
+     * @param model model 객체
+     * @return 정보 페이지
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
+    @GetMapping("/get/info")
+    public String getNoticeInfo(@RequestParam String noticeId, HttpServletRequest request, HttpServletResponse response, Model model){
 
+        //1. 사용자 세션 (MSTR) 유효성 검사
         HttpSession httpSession = request.getSession(true);
         String mstrSessionId = (String) httpSession.getAttribute("mstrSessionId");
 
         //1. 세션 체크
-        try{
-            comUtils.sessionCheck(mstrSessionId, request, response);
-        }catch(CustomException ex){
-            throw ex;
-        }
+        comUtils.sessionCheck(mstrSessionId, request, response);
 
+        //2. 공지사항 정보 조회
         NoticeVO noticeInfo = noticeService.getNoticeInfo(noticeId);
 
+        //3. model 객체에 삽입
         model.addAttribute("noticeInfo", noticeInfo);
+
+        //4. 응답
         return "/admin/notice/info";
     }
 
 
-    @PostMapping("/noticeAddProc")
+    /**
+     * 공지사항 생성
+     * @Method Name   : noticeAddProc
+     * @Date / Author : 2023.12.01  이도현
+     * @param noticeInfo notice 정보 객체
+     * @param request request 객체
+     * @param response response 객체
+     * @return 성공 유무
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
+    @PostMapping("/add")
     @ResponseBody
     public ResVO noticeAddProc(@RequestBody NoticeVO noticeInfo, HttpServletRequest request, HttpServletResponse response){
 
         //1. 사용자 세션 (MSTR) 유효성 검사
-
         HttpSession httpSession = request.getSession(true);
         String mstrSessionId = (String) httpSession.getAttribute("mstrSessionId");
+
+        //2 사용자 정보 파싱
         String userId = (String) httpSession.getAttribute("userId");
 
-        //1. 세션 체크
-        try{
-            comUtils.sessionCheck(mstrSessionId, request, response);
-        }catch(CustomException ex){
-            throw ex;
-        }
+        //2-1. 세션 체크
+        comUtils.sessionCheck(mstrSessionId, request, response);
 
+        //2. 공지사항 추가
         ResVO result = noticeService.addNotice(noticeInfo, userId);
         return result;
     }
 
-
-    @PostMapping("/noticeModifyProc")
+    /**
+     * 공지사항 수정
+     * @Method Name   : noticeModifyProc
+     * @Date / Author : 2023.12.01  이도현
+     * @param noticeInfo group 정보 객체
+     * @param request request 객체
+     * @param response response 객체
+     * @return 성공 유무
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
+    @PostMapping("/modify")
     @ResponseBody
     public ResVO noticeModifyProc(@RequestBody NoticeVO noticeInfo, HttpServletRequest request, HttpServletResponse response){
 
         //1. 사용자 세션 (MSTR) 유효성 검사
-
         HttpSession httpSession = request.getSession(true);
         String mstrSessionId = (String) httpSession.getAttribute("mstrSessionId");
 
         //1. 세션 체크
-        try{
-            comUtils.sessionCheck(mstrSessionId, request, response);
-        }catch(CustomException ex){
-            throw ex;
-        }
+        comUtils.sessionCheck(mstrSessionId, request, response);
 
         ResVO result = noticeService.modifyNotice(noticeInfo);
         return result;
     }
 
-    @PostMapping("/noticeDelProc")
+    /**
+     * 공지사항 삭제
+     * @Method Name   : noticeDelProc
+     * @Date / Author : 2023.12.01  이도현
+     * @param noticeInfo notice 정보 객체
+     * @param request request 객체
+     * @param response response 객체
+     * @return 성공 유무
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
+    @PostMapping("/delete")
     @ResponseBody
-    public ResVO noticeDelProc(@RequestBody Map<String,String> noticeInfo, HttpServletRequest request, HttpServletResponse response) throws WebObjectsException {
+    public ResVO noticeDelProc(@RequestBody Map<String,String> noticeInfo, HttpServletRequest request, HttpServletResponse response){
         //1. 사용자 세션 (MSTR) 유효성 검사
         String noticeId = noticeInfo.get("noticeId");
 
@@ -127,22 +211,29 @@ public class NoticeController {
         String mstrSessionId = (String) httpSession.getAttribute("mstrSessionId");
 
         //1. 세션 체크
-        try{
-            comUtils.sessionCheck(mstrSessionId, request, response);
-        }catch(CustomException ex){
-            throw ex;
-        }
+        comUtils.sessionCheck(mstrSessionId, request, response);
 
-        ResVO result = noticeService.delNotice(noticeId);
+        ResVO result = noticeService.deleteNotice(noticeId);
         return result;
     }
 
-    @GetMapping("/getNoticePopupList")
+    /**
+     * 공지사항 팝업 리스트 조회
+     * @Method Name   : getNoticePopupList
+     * @Date / Author : 2023.12.01  이도현
+     * @param request request 객체
+     * @param response response 객체
+     * @return 성공 유무
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
+    @GetMapping("/get/popup/list")
     @ResponseBody
-    public List<String> noticePopupList(HttpServletRequest request, Model model, HttpServletResponse response) throws WebObjectsException {
+    public List<String> getNoticePopupList(HttpServletRequest request, HttpServletResponse response){
 
         //1. 사용자 세션 (MSTR) 유효성 검사
-
         HttpSession httpSession = request.getSession(true);
         String mstrSessionId = (String) httpSession.getAttribute("mstrSessionId");
 
@@ -154,23 +245,33 @@ public class NoticeController {
         return popupList;
     }
 
-    @GetMapping("/getNoticePopup")
-    public String getNoticePopup(@RequestParam String noticeId, HttpServletRequest request, HttpServletResponse response, Model model) throws WebObjectsException {
-        System.out.println(noticeId);
-        //1. 사용자 세션 (MSTR) 유효성 검사
+    /**
+     * 공지사항 팝업 조회
+     * @Method Name   : getNoticePopup
+     * @Date / Author : 2023.12.01  이도현
+     * @param noticeId group 정보 객체
+     * @param request request 객체
+     * @param response response 객체
+     * @return 성공 유무
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
+    @GetMapping("/get/popup")
+    public String getNoticePopup(@RequestParam String noticeId, HttpServletRequest request, HttpServletResponse response, Model model){
 
+        //1. 사용자 세션 (MSTR) 유효성 검사
         HttpSession httpSession = request.getSession(true);
         String mstrSessionId = (String) httpSession.getAttribute("mstrSessionId");
 
         //1. 세션 체크
-        try{
-            comUtils.sessionCheck(mstrSessionId, request, response);
-        }catch(CustomException ex){
-            throw ex;
-        }
+        comUtils.sessionCheck(mstrSessionId, request, response);
 
+        //2. 공지사항 팝업 정보 조회
         NoticeVO noticeInfo = noticeService.getNoticeInfo(noticeId);
 
+        //3. 응답
         model.addAttribute("noticeInfo", noticeInfo);
         return "/admin/notice/popup";
     }
--- src/main/java/com/inside/ibip/domain/admin/notice/service/NoticeService.java
+++ src/main/java/com/inside/ibip/domain/admin/notice/service/NoticeService.java
@@ -10,11 +10,21 @@ import com.microstrategy.web.objects.WebObjectsException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-
 import static com.inside.ibip.global.utils.ComUtils.isNullOrEmpty;
 
+/**
+ * @FileName     : NoticeService.java
+ * @Date         : 2023.12.01
+ * @Author       : 이도현
+ * @Description  : 공지사항 Service, 공지사항 리스트 조회, 정보 조회, 생성, 수정, 삭제
+ * @History
+ * =======================================================
+ *   DATE			AUTHOR			NOTE
+ * =======================================================
+ *   2023.12.01     이도현         최초작성
+ *
+ */
 @Log4j2
 @Service
 public class NoticeService {
@@ -25,21 +35,53 @@ public class NoticeService {
     @Autowired
     private MstrObject mstrObject;
 
+    /**
+     * 공지사항 리스트를 조회
+     * @Method Name   : getNoticeList
+     * @Date / Author : 2023.12.01  이도현
+     * @return 공지사항 객체 리스트
+     * @History
+     * 2023.12.01	최초생성
+     */
     public List<NoticeVO> getNoticeList() {
         List<NoticeVO> noticeList = noticeMapper.getNoticeList();
         return noticeList;
     }
 
+    /**
+     * 공지사항 수정
+     * @Method Name   : modifyNotice
+     * @Date / Author : 2023.12.01  이도현
+     * @param noticeInfo notice 정보 객체
+     * @return 성공 유무
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
     public ResVO modifyNotice(NoticeVO noticeInfo){
+
         int resultCnt = noticeMapper.modifyNotice(noticeInfo);
 
         if(!(resultCnt > 0)){
-            throw new CustomException(ResultCode.ETC_ERROR);
+            throw new CustomException(ResultCode.DB_ETC_ERROR);
         }
         return new ResVO(ResultCode.SUCCESS);
     }
 
+    /**
+     * 공지사항 정보 조회
+     * @Method Name   : getNoticeInfo
+     * @Date / Author : 2023.12.01  이도현
+     * @param noticeId 조회할 공지사항의 Id
+     * @return 그룹 객체
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
     public NoticeVO getNoticeInfo(String noticeId){
+
         NoticeVO noticeInfo = noticeMapper.getNotice(noticeId);
         return noticeInfo;
     }
@@ -49,33 +91,72 @@ public class NoticeService {
         return noticeInfo;
     }
 
+    /**
+     * 공지사항 팝업 리스트 조회
+     * @Method Name   : getNoticePopupList
+     * @Date / Author : 2023.12.01  이도현
+     * @return 성공 유무
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
     public List<String> getNoticePopupList(){
         List<String> noticePopupList = noticeMapper.getNoticePopupList();
         return noticePopupList;
     }
 
+    /**
+     * 공지사항 생성
+     * @Method Name   : addNotice
+     * @Date / Author : 2023.12.01  이도현
+     * @param noticeInfo group 정보 객체
+     * @param userId 사용자 Id
+     * @return 성공 유무
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
     public ResVO addNotice(NoticeVO noticeInfo, String userId){
+
+        //1. 제목 유효성 검사
         if(isNullOrEmpty(noticeInfo.getTitle())){
             throw new CustomException(ResultCode.INVALID_TITLE);
         }
+        //2. 본문 유효성 검사
         else if(isNullOrEmpty(noticeInfo.getContent())){
             throw new CustomException(ResultCode.INVALID_CONTENT);
         }
+
+        //3. 사용자 이름 추가
         noticeInfo.setWriter(mstrObject.getUserLoginName(userId));
 
+        //4. 공지사항 생성
         int resultCnt = noticeMapper.insertNotice(noticeInfo);
 
         if(!(resultCnt > 0)){
-            throw new CustomException(ResultCode.ETC_ERROR);
+            throw new CustomException(ResultCode.DB_ETC_ERROR);
         }
         return new ResVO(ResultCode.SUCCESS);
     }
 
-    public ResVO delNotice(String noticeId) throws WebObjectsException {
+    /**
+     * 공지사항 삭제
+     * @Method Name   : delNotice
+     * @Date / Author : 2023.12.01  이도현
+     * @param noticeId 삭제할 공지사항 정보
+     * @return 성공 유무
+     * @History
+     * 2023.12.01	최초생성
+     *
+     * @Description
+     */
+    public ResVO deleteNotice(String noticeId){
         int resultCnt = noticeMapper.deleteNotice(noticeId);
 
         if(!(resultCnt > 0)){
-            throw new CustomException(ResultCode.ETC_ERROR);
+            throw new CustomException(ResultCode.DB_ETC_ERROR);
         }
         return new ResVO(ResultCode.SUCCESS);
     }
